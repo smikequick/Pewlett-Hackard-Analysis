da49a7f70d09e7afa9a4713cadd9010991018f11
smikequick <87885677+smikequick@users.noreply.github.com>
2021-10-14 04:38:32 -0600

    updating SQL

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -157,3 +157,36 @@ INNER JOIN dept_employees AS de
 ON (ce.emp_no = de.emp_no)
 INNER JOIN departments AS d
 ON (de.dept_no = d.dept_no);
+
+--Create new table for retiring employees on the Sales team.
+SELECT e.emp_no, 
+	e.first_name, 
+	e.last_name,
+	e.gender,
+	s.salary,
+	de.to_date,
+	de.dept_no
+INTO retiring_emp_info_sales
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_employees as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01')
+AND (dept_no = 'd007');
+
+--Create new table for retiring employees on the Sales team.
+SELECT e.emp_no, 
+	e.first_name, 
+	e.last_name,
+	de.to_date,
+	de.dept_no
+INTO retiring_emp_info_sales_dev
+FROM employees as e
+INNER JOIN dept_employees as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31') AND dept_no IN ('d005', 'd007')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01');
